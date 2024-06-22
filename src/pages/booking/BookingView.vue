58dd31c22a75c0f0ef3b3024fbf87711ef0defd5
JFCarrizo <francarrizo2001gmail.com>
2024-06-22 14:13:42 -0300

    se añadio a la pantalla de reserva la funcionalidad para agregar nuevas reservas

--- src/pages/booking/BookingView.vue
+++ src/pages/booking/BookingView.vue
@@ -1,35 +1,135 @@
 <template>
+  <Toolbar class="mb-6">
+    <template #start>
+      <Button label="Nueva" icon="pi pi-plus" severity="success" class="mr-2" @click="openNew" />
+      <Button label="Editar" icon="pi pi-plus" severity="success" class="mr-2" @click="openNew" />
+      <Button label="Borrar" icon="pi pi-trash" severity="danger" @click="confirmDeleteSelected"
+        :disabled="!selectedBookings || !selectedBookings.length" />
+    </template>
+  </Toolbar>
+
   <div class="grid">
     <div class="col-12 xl:col-12">
       <div class="card">
         <h5>Reservas</h5>
-        <DataTable :value="bookings" removableSort  :rows="5" :paginator="true" responsiveLayout="scroll">
-          <Column
-            field="guestName"
-            header="Nombre de rWeservista"
-            :sortable="true"
-            style="width: 15%"
-          ></Column>
-          <Column field="admissionDate" header="Ingreso" :sortable="true" style="width: 25%">
+        <DataTable v-model:selection="selectedBookings" v-model:filters="filters" :value="bookings"
+          selectionMode="single" dataKey="id" removableSort :rows="5" :paginator="true" responsiveLayout="scroll"
+          filterDisplay="row" :loading="loading">
+          <template #empty> No customers found. </template>
+          <template #loading> Loading customers data. Please wait. </template>
+
+          <Column field="guestName" header="Nombre de reservista" :sortable="true" style="width: 15%">
+            <template #body="{ data }">
+              {{ data.guestName }}
+            </template>
+            <template #filter="{ filterModel, filterCallback }">
+              <InputText v-model="filterModel.value" type="text" @input="filterCallback()"
+                placeholder="Buscar por nombre" />
+            </template>
           </Column>
-          <Column field="exitDate" header="Salida" :sortable="true" style="width: 25%"></Column>
-          <Column field="housingName" header="Cabaña" :sortable="true" style="width: 25%"> </Column>
-          <Column field="numberGuests" header="Inquilinos" :sortable="true" style="width: 10%"></Column>
+
+          <Column field="admissionDate" header="Ingreso" :sortable="true" style="width: 25%">
+            <template #body="{ data }">
+              {{ data.admissionDate }}
+            </template>
+            <template #filter="{ filterModel, filterCallback }">
+              <InputText v-model="filterModel.value" type="text" @input="filterCallback()" placeholder="Buscar fecha" />
+            </template>
+          </Column>
+
+          <Column field="exitDate" header="Salida" :sortable="true" style="width: 25%">
+            <template #body="{ data }">
+              {{ data.admissionDate }}
+            </template>
+            <template #filter="{ filterModel, filterCallback }">
+              <InputText v-model="filterModel.value" type="text" @input="filterCallback()" placeholder="Buscar fecha" />
+            </template>
+          </Column>
+
+          <Column field="housingName" header="Cabaña" :sortable="true" style="width: 25%"> <template #body="{ data }">
+              {{ data.housingName }}
+            </template>
+            <template #filter="{ filterModel, filterCallback }">
+              <InputText v-model="filterModel.value" type="text" @input="filterCallback()" placeholder="Hospedaje" />
+            </template>
+          </Column>
+
+          <Column field="numberGuests" header="Inquilinos" :sortable="true" style="width: 10%"> <template
+              #body="{ data }">
+              {{ data.numberGuests }}
+            </template>
+            <template #filter="{ filterModel, filterCallback }">
+              <InputText v-model="filterModel.value" type="text" @input="filterCallback()" placeholder="Inquilinos" />
+            </template>
+          </Column>
         </DataTable>
       </div>
     </div>
   </div>
+
+  <Dialog v-model:visible="productDialog" :style="{ width: '300px' }" header="Detalle reserva" :modal="true">
+    <div class="ml-3">
+      <div class="field">
+        <label for="calendar" class="w-full">Rango de fechas</label>
+        <Calendar id="calendar" v-model="booking.dates" selectionMode="range" :manualInput="false" class="w-full" />
+      </div>
+
+      <div class="field">
+        <label for="guest" class="w-full">Eleji el inquilino</label>
+        <AutoComplete id="guest" v-model="booking.guest" dropdown :suggestions="guests" optionLabel="name"
+          @complete="search" class="w-full" />
+      </div>
+
+      <div class="field">
+        <label for="housing" class="w-full">Eleji la cabaña</label>
+        <Dropdown id="housing" v-model="booking.housing" :options="housings" optionLabel="name"
+          placeholder="Selecciona una cabaña" class="w-full" />
+      </div>
+    </div>
+
+
+    <template #footer>
+      <Button label="Cancel" icon="pi pi-times" text @click="hideDialog" />
+      <Button label="Save" icon="pi pi-check" @click="saveBooking" />
+    </template>
+  </Dialog>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue';
-import { getBookingData } from '../../services/booking.service.js';
-import { getGuestData } from '../../services/guest.service.js';
-import { getHousingData } from '../../services/housing.service.js';
+import { createBooking, getBookingData } from '../../services/booking.service';
+import { getGuestData } from '../../services/guest.service';
+import { FilterMatchMode } from 'primevue/api'
+import { getHousingData } from '../../services/housing.service';
 
 onMounted(() => {
-  mapData().then((data) => (bookings.value = data));
+
+  mapData().then((data) => {
+    bookings.value = data;
+    loading.value = false;
+  });
+
+  getHousingData().then((housingData) => {
+    housings.value = housingData.map(housing => { return { id: housing.id, name: housing.nombre } })
+  })
+
 });
+const filters = ref({
+  guestName: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+  admissionDate: { value: null, matchMode: FilterMatchMode.CONTAINS },
+  exitDate: { value: null, matchMode: FilterMatchMode.CONTAINS },
+  numberGuests: { value: null, matchMode: FilterMatchMode.EQUALS },
+  housingName: { value: null, matchMode: FilterMatchMode.CONTAINS }
+});
+const loading = ref()
+const booking = ref({});
+const productDialog = ref(false);
+const submitted = ref(false);
+const housings = ref()
+const guests = ref({})
+const bookings = ref();
+
+
 
 async function mapData() {
   const bookingData = await getBookingData();
@@ -47,5 +147,31 @@ async function mapData() {
   });
 }
 
-const bookings = ref();
+const openNew = () => {
+  booking.value = {};
+  submitted.value = false;
+  productDialog.value = true;
+};
+
+const saveBooking = async () => {
+  const bookingDetails = booking.value
+  console.log(bookingDetails)
+  await createBooking(bookingDetails.dates[0], bookingDetails.dates[1], bookingDetails.guest.id, bookingDetails.housing.id)
+  await mapData()
+  productDialog.value= false;
+  submitted.value = true;
+}
+
+
+const search = async (event) => {
+  const guestsData = await getGuestData();
+  const filteredGuest = guestsData.filter((guest) => {
+    return guest.nombre.toLowerCase().startsWith(event.query.toLowerCase())
+  })
+
+  guests.value = filteredGuest.map((guest) => {
+    return { id: guest.id, name: guest.nombre }
+  })
+
+}
 </script>
--- src/services/booking.service.js
+++ src/services/booking.service.js
@@ -1,6 +1,21 @@
-import axios from "axios";
+import axios from 'axios';
 
-export async function getBookingData(){
-    const response = await axios.get("http://localhost:3000/reserva")
-    return response.data
-}   
+export async function getBookingData() {
+  const response = await axios.get('http://localhost:3000/reserva');
+  return response.data;
+}
+
+export async function createBooking(startDate, endDate, guestId, housingId) {
+
+  const data = {
+    reserva: {
+        id_inquilino: guestId,
+        id_hospedaje: housingId,
+        fecha_ingreso: startDate,
+        fecha_egreso: endDate
+    },
+    acompanantes: []
+  };
+  console.log(data)
+  const respoonse = await axios.post('http://localhost:3000/reserva', data);
+}
