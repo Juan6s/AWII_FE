e0e13acac248777bb10755a2bdd8ee009efcd687
Juan Francisco Carrizo <francarrizo2001@gmail.com>
2024-07-05 16:17:04 -0300

    añade toolbar para añadir inquilinos

--- src/pages/guest/GuestView.vue
+++ src/pages/guest/GuestView.vue
@@ -1,4 +1,12 @@
 <template>
+  <Button
+    label="Nuevo inquilino"
+    icon="pi pi-plus"
+    severity="success"
+    class="mr-2 mb-4"
+    @click="openNew"
+  />
+
   <div class="grid">
     <div class="col-4" v-for="guest in guests" :key="guest.id">
       <Card class="p-mb-3">
@@ -6,29 +14,65 @@
           {{ guest.name.toUpperCase() }}
         </template>
         <template #content>
-          <p>
-            <b>Telefono:</b> {{ guest.phone }}
-          </p>
-          <p>
-            <b>DNI:</b> {{ guest.dni }}
-          </p>
-
+          <p><b>Telefono:</b> {{ guest.phone }}</p>
+          <p><b>DNI:</b> {{ guest.dni }}</p>
         </template>
       </Card>
     </div>
   </div>
+
+  <Dialog
+    v-model:visible="submitGuestDialog"
+    :style="{ width: '300px', height: '700px' }"
+    header="Detalle inquilino"
+    :modal="true"
+  >
+    <div class="ml-3">
+      <div class="field">
+        <label for="name" class="w-full">Nombre</label>
+        <InputText id="name" v-model="newGuest.name" />
+      </div>
+      <div class="field">
+        <label for="dni" class="w-full">DNI</label>
+        <InputText id="dni" v-model="newGuest.dni" />
+      </div>
+      <div class="field">
+        <label for="phone" class="w-full">Telefono</label>
+        <InputMask
+          id="phone"
+          v-model="newGuest.phone"
+          mask="(999) 999-9999"
+          placeholder="(999) 999-9999"
+          fluid
+        />
+      </div>
+    </div>
+
+    <template #footer>
+      <Button label="Save" icon="pi pi-check" @click="saveGuest" />
+    </template>
+  </Dialog>
 </template>
 
 <script setup>
-import { getGuestData } from '@/services/guest.service';
+import { getGuestData, createGuest } from '@/services/guest.service';
 import { onMounted, ref } from 'vue';
 
+const submitGuestDialog = ref(false);
+const guests = ref();
+const newGuest = ref({});
 
 onMounted(async () => {
   guests.value = await getGuestData();
-})
+});
 
-const guests = ref()
+const openNew = () => {
+  submitGuestDialog.value = true;
+};
 
-
+const saveGuest =async() => {
+  const {name, dni, phone} = newGuest.value
+  await createGuest(name, phone, dni)
+  submitGuestDialog.value = false;
+}
 </script>
--- src/services/guest.service.js
+++ src/services/guest.service.js
@@ -1,9 +1,20 @@
-import axios from "../config/request.config"
+import axios from '../config/request.config';
 
-import { getToken } from "./auth.service";
+import { getToken } from './auth.service';
 
-export async function getGuestData(){
-    const token = await getToken()
-    const response = await axios.get("http://localhost:3000/inquilinos", {headers: {Authorization: token}})
-    return response.data
-}       
+export async function getGuestData() {
+  const token = await getToken();
+  const response = await axios.get('http://localhost:3000/inquilinos', {
+    headers: { Authorization: token }
+  });
+  return response.data;
+}
+
+export async function createGuest(name, phone, dni) {
+  const token = await getToken();
+  const response = await axios.post(
+    'http://localhost:3000/inquilinos',
+    { name, phone, dni },
+    { headers: { Authorization: token } }
+  );
+}
